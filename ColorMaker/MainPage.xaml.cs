3d9007340a675865e336e4622505527f02e2d299
Bryan Jesus Rivera Vidal <bryan.jesus.rv@gmail.com>
2022-08-14 22:53:19 -0500

    Cambios 14/08/2022

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿namespace ColorMaker;
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
-	
+	bool isRandom;
+	string hexValue;
 	public MainPage()
 	{
 		InitializeComponent();
@@ -10,21 +13,49 @@ public partial class MainPage : ContentPage
 
 	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
 	{
-		var red = sldRed.Value;
-		var green = sldGreen.Value;
-		var blue = sldBlue.Value;
+		if (!isRandom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
 
-		Color color = Color.FromRgb(red, green, blue);
+            Color color = Color.FromRgb(red, green, blue);
 
-		SetColor(color);
+            SetColor(color);
+        }
+		
 	}
 
 	private void SetColor(Color color)
 	{
 		btnRandom.BackgroundColor = color;
 		Container.BackgroundColor = color;
-		lblHex.Text = color.ToHex();
+        hexValue = color.ToHex();
+        lblHex.Text = hexValue;
 	}
 
+	private void btnRandom_Clicked(object sender, EventArgs e)
+	{
+		isRandom = true;
+		var rand = new Random();
+		var color = Color.FromRgb
+			(rand.Next(0, 256),
+			rand.Next(0, 256),
+			rand.Next(0, 256));
+
+		SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+		isRandom = false;
+	}
+
+	private async void ImageButton_Clicked(object sender, EventArgs e)
+	{
+		await Clipboard.SetTextAsync(hexValue);
+		var toast = Toast.Make("Copied", CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+		await toast.Show();
+	}
 }
 
